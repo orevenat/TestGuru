9559c24cd620010337403b93cc3d3a8ce61a4f1b
orevenat <agrry@yandex.ru>
2019-02-15 17:05:24 +0500

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_115614) do
+ActiveRecord::Schema.define(version: 2019_02_15_120424) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
-    t.boolean "correct", null: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
