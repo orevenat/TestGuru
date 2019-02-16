a096e56d5c9de7f82ab34c9eea2e8efa996bccfb
orevenat <agrry@yandex.ru>
2019-02-16 22:47:37 +0500

    joins v2

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,10 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.where_category(name)
-    query = "INNER JOIN categories ON category_id = categories.id " \
-            "AND categories.title = '#{name}'"
-    Test.joins(query).order(title: :desc).pluck(:title)
+    query = 'INNER JOIN categories ON category_id = categories.id'
+    Test
+      .joins(query)
+      .where(categories: { title: name })
+      .order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def find_test(level)
-    query = "JOIN user_tests ON test_id = tests.id AND user_id = #{id}"
-    Test.where(level: level).joins(query)
+    query = 'JOIN user_tests ON test_id = tests.id'
+    Test.where(level: level).joins(query).where(user_tests: { user_id: id })
   end
 end
