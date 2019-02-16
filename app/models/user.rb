07acb953265a4f44c21d647b39ff7e6e0323f574
orevenat <agrry@yandex.ru>
2019-02-16 17:39:03 +0500

    Add find_test method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  def find_test(level)
+    tests = UserTest.where(user_id: id).pluck(:test_id)
+    Test.where(id: tests).where(level: level)
+  end
 end
