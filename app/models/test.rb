3a5bd32c7bfe5249893879bf9e2f63986109038b
orevenat <agrry@yandex.ru>
2019-02-16 17:48:28 +0500

    add Test where_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.where_category(name)
+    category_id = Category.find_by(title: name).id
+
+    Test.where(category_id: category_id).order(title: :DESC)
+  end
 end
