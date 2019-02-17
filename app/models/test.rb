f514bccd8ac0a9d31fa38bd3a9b3c9727a300238
orevenat <agrry@yandex.ru>
2019-02-17 08:05:30 +0500

    Fix naming & join

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  def self.where_category(name)
-    query = 'INNER JOIN categories ON category_id = categories.id'
-    Test
-      .joins(query)
-      .where(categories: { title: name })
-      .order(title: :desc).pluck(:title)
+  def self.by_category(name)
+    Test.joins(:category)
+        .where(categories: { title: name })
+        .order(title: :desc).pluck(:title)
   end
 end
