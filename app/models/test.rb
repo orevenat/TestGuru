b5b65fbbce20178bc8ea1b779a074c296c12d039
orevenat <agrry@yandex.ru>
2019-02-20 17:49:11 +0500

    Add scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,17 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  scope :easy, -> { where(level: 1..2) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, lamda(name) do
+    category.where(categories: { title: name })
+            .order(title: :desc)
+            .pluck(:title)
+  end
+
   validates :title, presence: true
   validates :level, numericality: { greater_than_or_equal_to: 0 }
   validates :title, uniqueness: { scope: :level,
                                   message: 'title + level must be unique' }
-
-  def self.by_category(name)
-    Test.joins(:category)
-        .where(categories: { title: name })
-        .order(title: :desc).pluck(:title)
-  end
 end
