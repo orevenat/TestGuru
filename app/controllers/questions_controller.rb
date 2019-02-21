754812284ecc3124ee9fe8128512d4ce827d11f2
orevenat <agrry@yandex.ru>
2019-02-21 23:36:02 +0500

    Add create(without save to model)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,11 +10,19 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @test = Test.find(params[:test_id])
   end
 
   def create
+    render plain: question_params.inspect
   end
 
   def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 end
