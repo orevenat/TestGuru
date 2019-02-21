f115ecb91c7662ccadd55a6c242942e9ebe90197
orevenat <agrry@yandex.ru>
2019-02-21 23:58:29 +0500

    Add question rescue_from

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,8 @@ class QuestionsController < ApplicationController
 
   before_action :find_test
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     @questions = @test.questions
   end
@@ -27,6 +29,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
   end
 
   def question_params
