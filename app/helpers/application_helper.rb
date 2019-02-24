9dc62ec0e5818dfaf1429a7811d5c051dbe9864f
orevenat <agrry@yandex.ru>
2019-02-24 20:08:14 +0500

    Add helpers and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo = '')
+    "<a href='https://github.com/#{author}/#{repo}' target='_blank'>
+      #{repo != '' ? repo : author}
+    </a>".html_safe
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header(type, title)
+    message = type == :create ? 'Create new' : 'Edit'
+    "#{message} #{title} Question"
+  end
 end
