f89237ab0809afb6bf606a0ae8756032ed329722
orevenat <agrry@yandex.ru>
2019-02-26 11:27:28 +0500

    Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root to: 'tests#index'
+
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
+
 end
