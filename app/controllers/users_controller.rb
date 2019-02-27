63bf05b4c825d2617e722cc635c5de540c9aeda9
orevenat <agrry@yandex.ru>
2019-02-27 20:16:51 +0500

    Add session id after register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to tests_path
     else
       render :new
@@ -17,7 +18,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :first_name, :last_name, :password,
+                                 :password_confirmation)
   end
 
 end
