dfe63f700061dfd73313cc518507d581419e0103
orevenat <agrry@yandex.ru>
2019-03-07 14:47:29 +0500

    remove console.log

--- app/assets/javascripts/utilities/password_checking.js
+++ app/assets/javascripts/utilities/password_checking.js
@@ -13,7 +13,6 @@ document.addEventListener('turbolinks:load', function () {
 });
 
 function checkPasswords(field1, field2) {
-  console.log(12333);
   if (field1.value !== field2.value && field2.value.length > 0) {
     field1.classList.add('is-invalid');
     field2.classList.add('is-invalid');
