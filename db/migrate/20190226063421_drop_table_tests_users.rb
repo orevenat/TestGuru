f2aa5e26333ee1622705231fdecc37ebb60c93b8
orevenat <agrry@yandex.ru>
2019-03-14 15:23:26 +0500

    migrations fixes

--- db/migrate/20190226063421_drop_table_tests_users.rb
+++ db/migrate/20190226063421_drop_table_tests_users.rb
@@ -2,4 +2,10 @@ class DropTableTestsUsers < ActiveRecord::Migration[5.2]
   def up
     drop_table :tests_users, if_exists: true
   end
+
+  def down
+    create_join_table :users, :tests do |t|
+      t.index [:user_id, :test_id], unique: true
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_085654) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "question_id"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 2019_03_12_085654) do
   end
 
   create_table "gists", force: :cascade do |t|
-    t.integer "question_id"
-    t.integer "user_id"
+    t.bigint "question_id"
+    t.bigint "user_id"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,16 +42,16 @@ ActiveRecord::Schema.define(version: 2019_03_12_085654) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "test_id"
+    t.bigint "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "test_passages", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "test_id"
-    t.integer "current_question_id"
+    t.bigint "user_id"
+    t.bigint "test_id"
+    t.bigint "current_question_id"
     t.integer "correct_questions", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_085654) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "category_id"
+    t.bigint "category_id"
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -99,4 +99,12 @@ ActiveRecord::Schema.define(version: 2019_03_12_085654) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "gists", "questions"
+  add_foreign_key "gists", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
+  add_foreign_key "tests", "categories"
 end
