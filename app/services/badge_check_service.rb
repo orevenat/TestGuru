d218a7f70bb897d9c931d6c573e19ffce930e02e
orevenat <agrry@yandex.ru>
2019-03-25 16:40:18 +0500

    change service

--- app/services/badge_check_service.rb
+++ app/services/badge_check_service.rb
@@ -1,11 +1,27 @@
 class BadgeCheckService
-  def initialize(user)
+  def initialize(user, test_passage)
     @user = user
-    @badges = Badge.all
-    @checks = []
+    @test_passage = test_passage
+    @rules = Rule.all
   end
 
-  def call(test_passage)
+  def call
+    @rules.each do |rule|
+      @current_user.badges << rule.badges if send "#{rule.rule_type}_award?", rule
+    end
+  end
 
+  private
+
+  def category_award?(rule)
+    TestPassages.where(user_id: @current_user.id).select {|tp| tp.test.category == rule.category && tp.success? } == Test.where(category_id: rule.value.to_i)
+  end
+
+  def level_award?(rule)
+    Test.where(level: rule.value.to_i).count == TestPassage.where(user_id: @current_user.id).select { |tp| tp.test.level == 3 && tp.success? }.count
+  end
+
+  def attepmt_award?(_rule)
+    @test_passage.find_by(user_id: @current_user.id).count == 1
   end
 end
